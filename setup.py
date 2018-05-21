95e11450a8d72117ff298b14a06ad422349dc0d0
Joerg <joerg.doepfert@flixbus.com>
2018-05-21 13:51:12 +0200

    add queue package to setup.py

--- setup.py
+++ setup.py
@@ -6,6 +6,6 @@ setup(
     name='mediaire python toolbox',
     version=mediaire_toolbox.__version__,
     maintainer='joerg doepfert',
-    packages=['mediaire_toolbox'],
+    packages=['mediaire_toolbox', 'mediaire_toolbox.queue'],
     long_description=open('README.md').read(),
 )
