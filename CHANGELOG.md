beff8fb24dce99077f6e01269a874ca0fe826a09
Joerg <joerg.doepfert@flixbus.com>
2018-05-21 16:52:22 +0200

    update requirements and setup.py, create changelog

---
+++ CHANGELOG.md
@@ -0,0 +1,6 @@
+# Changelog
+
+## [unreleased] - 2018-XX-XX
+
+## [0.1.0] - 2018-05-21
+* add redis work queue prototype
--- setup.py
+++ setup.py
@@ -3,9 +3,10 @@ from setuptools import setup
 import mediaire_toolbox
 
 setup(
-    name='mediaire python toolbox',
+    name='mediaire_toolbox',
     version=mediaire_toolbox.__version__,
     maintainer='joerg doepfert',
-    packages=['mediaire_toolbox', 'mediaire_toolbox.queue'],
+    packages=['mediaire_toolbox',
+              'mediaire_toolbox.queue'],
     long_description=open('README.md').read(),
 )
