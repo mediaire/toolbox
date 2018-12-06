2e7c4c952e56e2fc933d621f2061b08b15588c8f
Pere <p.ferrera@mediaire.de>
2018-12-06 16:16:08 +0100

    WIP

--- mediaire_toolbox/transaction_db/model.py
+++ mediaire_toolbox/transaction_db/model.py
@@ -10,7 +10,7 @@ Base = declarative_base()
 """Metadata that indicates the version of the schema in the database.
 We use our own schema migration routines. Don't forget to change the version
 number here if you add more fields to the Transaction object, and implement
-the migration SQL queries in transaction_db.migrate"""
+the migration SQL queries in migrations.py"""
 SCHEMA_NAME = "TRANSACTION"
 SCHEMA_VERSION = 2
 
