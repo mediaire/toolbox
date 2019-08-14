4fd8f93f3f35bc0c4b6d09534cced39941b9d66c
Wenxin Ling <w.ling@mediaire.de>
2019-08-14 09:54:03 +0200

    enfor merge request title format

---
+++ scripts/enforce_merge_request.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+TITLE_CHECK=false
+echo "$CI_MERGE_REQUEST_TITLE"
+case "$CI_MERGE_REQUEST_TITLE" in
+    *"[MAJOR]"*) TITLE_CHECK=true
+    ;;
+    *"[MINOR]"*) TITLE_CHECK=true
+    ;;
+    *"[PATCH]"*) TITLE_CHECK=true
+    ;;
+esac
+if ! $TITLE_CHECK; then
+    echo "Merge request format is wrong, add [MAJOR], [MINOR] or [PATCH] to title"
+    exit 1
+else
+    echo "Test passed"
+    exit 0
+fi
